a3e08714f2ca197fe61f33df52ee146b653316f5
EduardaAndrade03 <eduardaamaria2608@gmail.com>
2026-05-24 14:43:49 -0300

    concertando rota dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\ClientController;
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\SaleController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,11 +13,9 @@ Route::get('/', function () {
 Route::prefix('hermit-purple')->group(function () { // "/hermit-purple/"
 
     Route::prefix('admin')->group(function () { // "/hermit-purple/admin"
-        Route::get('/dashboard', function () { // "/hermit-purple/admin/dashboard"
-            return view();
-        });
+        Route::get('/dashboard', [DashboardController::class, 'index']);
         Route::resource('/products', ProductController::class);
         Route::resource('/clients', ClientController::class);
-        Route::resource('/sale', SaleController::class);
+        Route::resource('/sales', SaleController::class);
     });
 });
